17d9da08ed19675abb724b552fe9e4551ec415ef
Reetika-786 <reetika848@gmail.com>
2024-10-13 10:59:32 +0530

    dom 4th project

---
+++ imagehover.js
@@ -0,0 +1,17 @@
+var elem1 = document.querySelectorAll(".elem");
+
+elem1.forEach(function(val){
+
+    val.addEventListener('mousemove', function(dets){
+        val.childNodes[3].style.left = dets.x+"px";
+        val.childNodes[3].style.top = dets.y+"px";
+    })
+
+    val.addEventListener('mouseenter', function(dets){
+        val.childNodes[3].style.opacity = 1;
+    })
+
+    val.addEventListener('mouseleave', function(dets){
+        val.childNodes[3].style.opacity = 0;
+    })
+})
